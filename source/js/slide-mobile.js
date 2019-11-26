500c6231d44252f9f59ed1adb08abc36e3dd7a35
Volodymyr <malones.vladimir@gmail.com>
2019-11-26 21:07:34 +0100

    Продолжаем вёрстку личного проекта

---
+++ source/js/slide-mobile.js
@@ -0,0 +1,26 @@
+var slider = document.querySelector(".example__slide");
+var photoBefore = slider.querySelector(".slide-example__photo--before");
+var photoAfter = slider.querySelector(".slide-example__photo--after");
+
+var btnBefore = slider.querySelector(".slide-example__toggle--before");
+var btnAfter = slider.querySelector(".slide-example__toggle--after");
+
+var indicator = slider.querySelector(".slide-example__indicator");
+
+btnBefore.addEventListener("click", function (evt) {
+  evt.preventDefault();
+});
+
+btnAfter.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  photoAfter.classList.add("slide-example__photo--visible");
+  photoBefore.classList.add("slide-example__photo--invisible");
+  indicator.classList.add("slide-example__indicator--on");
+});
+
+btnBefore.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  photoAfter.classList.remove("slide-example__photo--visible");
+  photoBefore.classList.remove("slide-example__photo--invisible");
+  indicator.classList.remove("slide-example__indicator--on");
+});

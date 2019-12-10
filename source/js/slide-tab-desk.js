c3c751bc4f231dd27eb541b3e97c5aa7d3bc474e
Volodymyr <malones.vladimir@gmail.com>
2019-12-10 16:15:06 +0200

    Улучшаем скорость загрузки

--- source/js/slide-tab-desk.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var slide = document.querySelector(".slide-example");
-var handle = slide.querySelector(".slide-example__handle-btn");
-
-var photoBefore = slide.querySelector(".slide-example__photo--before");
-var photoAfter = slide.querySelector(".slide-example__photo--after");

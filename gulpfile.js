231a95ab835d9b4235d54c81d43bd6013ca9bc8d
Volodymyr <malones.vladimir@gmail.com>
2019-12-13 15:31:55 +0200

    Защита проекта

--- gulpfile.js
+++ gulpfile.js
@@ -82,7 +82,7 @@ gulp.task("css", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff, woff2}",
+    "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
     "source/js/**",
     "source/*.ico"
